bfb96858a16627f8168af80e9fb8a8cd1755009f
Alexander DiMauro <alex@artkeh.com>
2016-01-12 01:53:43 -0500

    Weather reducer complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,17 @@
-import { API_KEY } from '../keys';
+import axios from 'axios';
+import { API_KEY } from '../../keys';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  console.log('Request:', request);
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
